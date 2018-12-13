9750c958c2ea9a6e34b14716fe69db237139346a
David Wagner <wagdav@gmail.com>
2018-12-13 21:39:02 +0100

    Extend the example with environment variables.

--- example/test.sh
+++ example/test.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 set -e
+echo "Artifacts from previous steps are available in this container"
 tree -L 1
+
+echo
+echo "Environment variables can be specified in the step: \$HELLO=$HELLO"
+
+echo
+echo "Running the output artifacts from the 'build' step"
 build/hello
