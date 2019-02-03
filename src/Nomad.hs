0f68195dbfd13fe9ee43f6a3dc0e6ecc808b9b11
David Wagner <wagdav@gmail.com>
2019-02-03 20:16:40 +0100

    json

--- src/Nomad.hs
+++ src/Nomad.hs
@@ -5,25 +5,34 @@ module Nomad
   )
 where
 
-import Nomad.Aeson
-import Nomad.Job
+import           Control.Lens
+import           Nomad.Aeson
+import           Nomad.Job
 
 import           Data.Aeson                     ( encode )
 import qualified Data.ByteString.Lazy          as B
 
-mkJob name image command volumes workDir sources = Job
-  name  -- id
-  name  -- name
-  "batch"
-  ["dc1"]
-  [TaskGroup name 1 [task]]
-  (ReschedulePolicy 0)
+mkJob name image command volumes workDir sources =
+  job
+    &  jobID .~ name
+    &  jobName .~ name
+    &  jobType .~ "batch"
+    &  jobTaskGroups
+    .~ [ taskGroup
+         &  taskGroupName .~ name
+         &  taskGroupTasks
+         .~ [ task
+            & taskName      .~ name
+            & taskDriver    .~ "docker"
+            & taskArtifacts .~ artifacts
+            & taskConfig . taskConfigImage .~ image
+            & taskConfig . taskConfigCommand .~ command
+            & taskConfig . taskConfigVolumes .~ volumes
+            & taskConfig . taskConfigWorkDir .~ workDir
+            ]
+       ]
  where
-  task =
-    Task name "docker" (TaskConfig image command volumes workDir) artifacts
   artifacts = [ Artifact s d | (s, d) <- sources ]
 
 writeJob :: FilePath -> Job -> IO ()
 writeJob p j = B.writeFile p (encode $ Nomad j)
-
-defaultJob = Job "" "" "" [] [] (ReschedulePolicy 0)
--- src/Nomad/Job.hs
+++ src/Nomad/Job.hs
@@ -14,20 +14,44 @@ data Job
   = Job
   { _jobID :: String
   , _jobName :: String
+   -- | https://www.nomadproject.io/api/json-jobs.html#type
   , _jobType :: String
+  -- | https://www.nomadproject.io/api/json-jobs.html#datacenters
   , _jobDatacenters :: [String]
+  -- | https://www.nomadproject.io/api/json-jobs.html#taskgroups
   , _jobTaskGroups :: [TaskGroup]
-  , _jobReschedulePolicy :: ReschedulePolicy
+  -- | https://www.nomadproject.io/api/json-jobs.html#reschedulepolicy
+  , _jobReschedulePolicy :: Maybe ReschedulePolicy
   }
  deriving (Show, Eq)
 
+job :: Job
+job = Job
+  { _jobName             = "example"
+  , _jobID               = "example"
+  , _jobType             = "service"
+  , _jobDatacenters      = ["dc1"]
+  , _jobTaskGroups       = []
+  , _jobReschedulePolicy = Nothing
+  }
+
 data TaskGroup
   = TaskGroup
+  -- | https://www.nomadproject.io/api/json-jobs.html#name
   { _taskGroupName :: String
+  -- | https://www.nomadproject.io/api/json-jobs.html#count
   , _taskGroupCount :: Int
+  -- | https://www.nomadproject.io/api/json-jobs.html#tasks
   , _taskGroupTasks :: [Task]
   }
  deriving (Show, Eq)
+
+taskGroup :: TaskGroup
+taskGroup = TaskGroup
+  { _taskGroupName  = "group"
+  , _taskGroupCount = 1
+  , _taskGroupTasks = []
+  }
 
 data ReschedulePolicy
   = ReschedulePolicy
@@ -38,18 +62,27 @@ data ReschedulePolicy
 data Task
   = Task
   { _taskName :: String
+  -- | https://www.nomadproject.io/api/json-jobs.html#driver
   , _taskDriver :: String
   , _taskConfig :: TaskConfig
   , _taskArtifacts :: [Artifact]
   }
  deriving (Show, Eq)
 
+task :: Task
+task = Task
+  { _taskName = "task"
+  , _taskDriver = "docker"
+  , _taskConfig = TaskConfig "" "" [] ""
+  , _taskArtifacts = []
+  }
+
 data TaskConfig
   = TaskConfig
   { _taskConfigImage :: String
   , _taskConfigCommand :: String
   , _taskConfigVolumes :: [String]
-  , _taskConfigWorkDir :: FilePath  -- FIXME JSON is work_dir !!
+  , _taskConfigWorkDir :: FilePath
   }
  deriving (Show, Eq)
 
@@ -62,7 +95,6 @@ data Artifact
 
 makeLenses ''Artifact
 makeLenses ''Job
-makeLenses ''Nomad
 makeLenses ''Task
 makeLenses ''TaskConfig
 makeLenses ''TaskGroup
