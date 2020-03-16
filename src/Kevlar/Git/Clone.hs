6520a242199097c1e198b435a0b222f48b92bae9
David Wagner <wagdav@gmail.com>
2020-03-16 21:09:31 +0100

    git: Don't change working directory
    It's horribly not thread safe.

--- src/Kevlar/Git/Clone.hs
+++ src/Kevlar/Git/Clone.hs
@@ -6,7 +6,7 @@ import Data.Set (Set)
 import qualified Data.Set as Set
 import GHC.Generics (Generic)
 import Kevlar.Git.Types
-import System.Directory
+import System.Directory (copyFile, createDirectoryIfMissing)
 import System.FilePath
 import System.Process (callProcess, readProcess)
 
@@ -15,30 +15,28 @@ clone :: Repository -> FilePath -> IO ()
 clone repo dest = do
   createDirectoryIfMissing True dest
   case repo of
-    WorkingCopy src -> withCurrentDirectory src (copyFiles dest)
+    WorkingCopy src -> copyFiles src dest
     Url url revision -> do
       callProcess "git" ["clone", url, dest]
-      gitReset revision
-  where
-    gitReset x =
-      withCurrentDirectory dest $
-        callProcess "git" ["reset", "--hard", x]
+      callProcess "git" ["-C", dest, "reset", "--hard", revision]
 
--- | Copy a Git source tree in the current directory to 'dstDir'
-copyFiles :: FilePath -> IO ()
-copyFiles dstDir = mapM_ copy =<< lsFiles
+-- | Copy a Git source tree from 'srcDir' to 'dstDir'
+copyFiles :: FilePath -> FilePath -> IO ()
+copyFiles srcDir dstDir = mapM_ copy =<< lsFiles srcDir
   where
     copy :: FilePath -> IO ()
     copy f = do
       createDirectoryIfMissing True (dstDir </> takeDirectory f)
-      copyFile f (dstDir </> f)
+      copyFile (srcDir </> f) (dstDir </> f)
 
-lsFiles :: IO [FilePath]
-lsFiles = do
-  tracked <- gitLs []
-  deleted <- gitLs ["--deleted"]
-  others <- gitLs ["--others", "--exclude-standard"]
+lsFiles :: FilePath -> IO [FilePath]
+lsFiles repo = do
+  tracked <- gitLs repo []
+  deleted <- gitLs repo ["--deleted"]
+  others <- gitLs repo ["--others", "--exclude-standard"]
   return $ Set.toList $ (tracked `Set.difference` deleted) `Set.union` others
 
-gitLs :: [String] -> IO (Set FilePath)
-gitLs args = Set.fromList . lines <$> readProcess "git" ("ls-files" : args) ""
+gitLs :: FilePath -> [String] -> IO (Set FilePath)
+gitLs repo args =
+  Set.fromList . lines
+    <$> readProcess "git" (["-C", repo, "ls-files"] ++ args) ""
